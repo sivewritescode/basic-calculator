09aa4d20f1328adf27a64aec79b007b8dcc65a03
Sive <siveloveslean@gmail.com>
2025-08-08 15:24:21 +0200

    Create functions that populate the display

--- main.js
+++ main.js
@@ -17,8 +17,13 @@ function divide(a, b) {
 
 //Variable for each part of an operation
 let firstNumber = "";
-let operator;
+let operatorSelected;
 let secondNumber = "";
+
+//State flags:
+let isOperatorSelected = false; //State flag for if an operator is clicked.
+let clearDisplay = false; //State for it the display should be cleared.
+let changeOperatorButtonColor = false;
 
 //Function that determines which operation is current
 function operate(a, operator, b) {
@@ -34,3 +39,42 @@ function operate(a, operator, b) {
   }
 }
 
+const display = document.querySelector("#calculator-display-text");
+
+const buttons = document.querySelectorAll(".number-button");
+
+//Event handler for when a number button is clicked.
+for (let button of buttons) {
+
+  button.addEventListener("click", () => {
+
+    //Code that determines whether firstNumber or secondNumber gets appended.
+    if (!isOperatorSelected) {
+      firstNumber += button.textContent; //Append firstNumber with number clicked.
+      display.textContent += button.textContent;
+    } else if (isOperatorSelected) {
+
+      //This removes the input for firstNumber before input for secondNumber is displayed.
+      if (clearDisplay) {
+        display.textContent = "";
+        clearDisplay = false;
+      }
+
+      secondNumber += button.textContent; 
+      display.textContent += button.textContent;
+    }
+  });
+
+}
+
+const operators = document.querySelectorAll(".operator-button");
+
+//Event handler for when an operator button is clicked.
+for (let operator of operators) {
+
+  operator.addEventListener("click", () => {
+    isOperatorSelected = true;
+    clearDisplay = true;
+    operatorSelected = operator.textContent;
+  });
+}
