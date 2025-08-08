b13dcbda07171b7c7a137f488aba8405747977e7
Sive <siveloveslean@gmail.com>
2025-08-08 12:24:22 +0200

    Create one function to handle operations

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Functions for all basic operations
 function add(a, b) {
   return a + b;
 }
@@ -13,3 +14,23 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+//Variable for each part of an operation
+let firstNumber = "";
+let operator;
+let secondNumber = "";
+
+//Function that determines which operation is current
+function operate(a, operator, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "÷":
+      return divide(a, b);
+  }
+}
+
