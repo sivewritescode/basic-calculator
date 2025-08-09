c553e7fc2c9aa566af14015e187600adfc6df2c2
Sive <siveloveslean@gmail.com>
2025-08-09 16:59:52 +0200

    Add clear button that wipes calculator's memory

--- main.js
+++ main.js
@@ -12,7 +12,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return a / b;
+  if (b === 0) {
+    return "ZeroDivisionError!";
+  } else {
+    return a / b;
+  }
 }
 
 //Variable for each part of an operation
@@ -94,9 +98,25 @@ const equalOperatorButton = document.querySelector("#equal-operator-button");
 
 //Function that handles an operation when the equal operator is clicked.
 equalOperatorButton.addEventListener("click", () => {
-  let resultOfOperation = operate(+firstNumber, operatorSelected, +secondNumber);
-  display.textContent = resultOfOperation;
-  firstNumber = resultOfOperation;
+
+  //Execute only when there is a full expression.
+  if (secondNumber) {
+    let resultOfOperation = operate(+firstNumber, operatorSelected, +secondNumber);
+    display.textContent = resultOfOperation;
+    firstNumber = resultOfOperation;
+    operatorSelected = "";
+    secondNumber = "";
+  }
+});
+
+const clearButton = document.querySelector("#clear-button");
+
+//Event handler for when the clear button is clicked.
+clearButton.addEventListener("click", () => {
+  display.textContent = "";
+  firstNumber = "";
   operatorSelected = "";
   secondNumber = "";
+  isOperatorSelected = false;
+  clearDisplay = false;
 });
